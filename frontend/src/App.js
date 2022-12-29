a8aca4a38508e750d9022d8673f281637baef376
iv_note <ivan_note@local.ru>
2022-12-29 23:58:24 +0100

    front setting install django-cors-headers для настройки CROS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,43 +2,59 @@ import React from 'react';
 import './logo.svg';
 import './App.css';
 import AuthorList from './components/Author';
+import axios from 'axios';
 
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      'authors' : []
+    constructor(props) {
+        super(props)
+        this.state = {
+            'authors': []
+        }
     }
-  }
 
 
-  componentDidMount() {
-    const authors = [
-                      {
-                      'first_name': 'Фёдор',
-                      'last_name': 'Достоевский',
-                      'birthday_year': 1821
-                      },
-                      {
-                      'first_name': 'Александр',
-                      'last_name': 'Грин',
-                      'birthday_year': 1880
-                      },
-                    ]
-    this.setState(
-        {
-              'authors': authors
-              }
-              )
-  }
-  render() {
-    return (
-        <div>
-          <AuthorList authors={this.state.authors} />
-        </div>
-    )
-  }
+    // componentDidMount() {
+    //     const authors = [
+    //         {
+    //             'first_name': 'Фёдор',
+    //             'last_name': 'Достоевский',
+    //             'birthday_year': 1821
+    //         },
+    //         {
+    //             'first_name': 'Александр',
+    //             'last_name': 'Грин',
+    //             'birthday_year': 1880
+    //         },
+    //     ]
+    //     this.setState(
+    //         {
+    //             'authors': authors
+    //         }
+    //     )
+    // }
+
+
+    componentDidMount() {
+        axios.get('http://127.0.0.1:8000/api/authors')
+            .then(response => {
+                const authors = response.data
+                this.setState(
+                    {
+                        'authors': authors
+                    }
+                )
+            }).catch(error => console.log(error))
+    }
+
+
+    render() {
+        return (
+            <div>
+                <AuthorList authors={this.state.authors}/>
+            </div>
+        )
+    }
 }
 
 export default App;
