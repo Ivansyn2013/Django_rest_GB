677e40a19bec78a728be7041395577ad092b491b
ivan_stand_l <ivansyn2013@yandex.ru>
2023-01-16 19:17:30 +0300

    finished

--- first_r_app/serializer.py
+++ first_r_app/serializer.py
@@ -1,6 +1,5 @@
 from rest_framework.serializers import HyperlinkedModelSerializer
-from .models import Author
-from .models import TODO
+from .models import Author, TODO, User, Project
 from rest_framework import serializers
 from datetime import datetime
 
@@ -10,10 +9,32 @@ class AuthorModelSerializer(HyperlinkedModelSerializer):
         model = Author
         fields = '__all__'
 
+
+class UserModelSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class ProjectModelSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+
+class AuthorModelSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = Author
+        fields = '__all__'
+
+
+
+
+
 class TODOHyperlinkSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = TODO
-        fields = '__all__'
+        #fields = '__all__'
         exclude = ['update_data',
                    'create_data',
                    ]
--- first_r_app/views.py
+++ first_r_app/views.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
-from .models import Author, TODO
+from .models import Author, TODO, User, Project
 from .serializer import AuthorModelSerializer
-from .serializer import TODOHyperlinkSerializer, TODOSerializer
+from .serializer import TODOHyperlinkSerializer, TODOSerializer, UserModelSerializer, ProjectModelSerializer
 
 class AuthorModelViewset(ModelViewSet):
     queryset = Author.objects.all()
@@ -10,3 +10,11 @@ class AuthorModelViewset(ModelViewSet):
 class TODOModelViewset(ModelViewSet):
     queryset = TODO.objects.all()
     serializer_class = TODOHyperlinkSerializer
+
+class UserModelViewset(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserModelSerializer
+
+class ProjectModelViewset(ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectModelSerializer
