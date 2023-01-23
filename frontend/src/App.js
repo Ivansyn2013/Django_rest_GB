7375ec7ca455345464894fd6d2d0fc57a2c6b6be
iv_note <ivan_note@local.ru>
2023-01-23 17:55:12 +0100

    start 5 dz

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,15 +6,15 @@ import Menu from './components/menu';
 import footer from "./components/footer";
 import TODOList from "./components/TODO";
 import axios from 'axios';
-
+import {HashRouter, Route} from "react-router-dom";
 
 class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
             'authors': [],
-            'menu':[],
-            'TODO':[],
+            'menu': [],
+            'TODO': [],
 
         }
     }
@@ -45,50 +45,42 @@ class App extends React.Component {
         axios.get('http://127.0.0.1:8000/api/authors')
             .then(response => {
                 const authors = response.data
+                const menu = [
+                    {'name': 'First'},
+                    {'name': 'Second'},
+                    {'name': 'Thid'},
+                ]
+
+
                 this.setState(
                     {
-                        'authors': authors
+                        'authors': authors,
+                        'menu': menu
                     }
                 )
             }).catch(error => console.log(error))
-
-        const menu = [
-            {'name':'First'},
-            {'name':'Second'},
-            {'name': 'Thid'},
-        ]
-
-        this.setState(
-            {
-                    'menu':menu,
-            })
-    }
-
-        axios.get('http://127.0.0.1:8000/api/TODO')
-            .then(response => {
-                const TODO = response.data
-                this.setState(
-                    {
-                        'TODO': TODO
-                    }
-                )
-            }).catch(error => console.log(error))
-
-
 
 
     render() {
         return (
+            <div className="App">
+                <HashRouter>
+                    <Route exact path='/' component={() => <AuthorList
+                        items={this.state.authors}/>}/>
+                    <Route exact path='/books' component={() => <BookList
+                        items={this.state.books}/>}/>
+                </HashRouter>
+            </div>
             <div>
                 <Menu menu={this.state.menu}/>
             </div>,
             <div>
                 <Menu menu={this.state.menu}/>
                 <AuthorList authors={this.state.authors}/>
-                <footer footer_l={this.state.menu}/>
+            <footer footer_l={this.state.menu}/>
             </div>
-        )
+    )
     }
-}
+    }
 
 export default App;
